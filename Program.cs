b3de39999832f08f778c1d757ab18149cc5db498
amirhoseinsazizavareh <158080807+amirhoseinsazizavareh@users.noreply.github.com>
2024-01-29 21:00:00 +0330

    Add files via upload
    امیرحسین سازی زواره

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace امیرحسین_سازی_زواره
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
